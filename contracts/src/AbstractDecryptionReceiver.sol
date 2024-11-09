04102335bce3172b0651b876f2f1d8a7c2cbd4cf
L0GYKAL.eth <32228897+L0GYKAL@users.noreply.github.com>
2024-11-09 04:51:45 +0100

    rename AbstractDecryptionReceiver (#3)

--- contracts/src/AbstractDecryptionReceiver.sol
+++ contracts/src/AbstractDecryptionReceiver.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.24;
 
 import {IDecryptionReceiver} from "./IDecryptionReceiver.sol";
 
-abstract contract AbstractRandomnessReceiver {
+abstract contract AbstractDecryptionReceiver {
     address public immutable DECRYPTION_PROVIDER = 0x4633bbdb16153B325bbcef4Baa770d718Eb552b8;
 
     error NotAuthorizedDecryptionProvider();
