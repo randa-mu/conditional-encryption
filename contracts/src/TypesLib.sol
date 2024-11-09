a94212204aa2ba541e9fd84ac76fb6444d4b17cb
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-11-09 12:41:54 +0100

    added some missing solidity

--- contracts/src/TypesLib.sol
+++ contracts/src/TypesLib.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import "./BLS.sol";
+
 library TypesLib {
      // Decryption request stores details for each decryption request
     struct DecryptionRequest {
@@ -14,9 +16,25 @@ library TypesLib {
 
     // Blocklock request stores details needed to generate blocklock decryption keys
     struct BlocklockRequest {
-        uint256 signatureRequestID;
+        uint256 decryptionRequestID;
         uint256 blockHeight;
+        Ciphertext ciphertext;
+        bytes signature;
+        address callback;
+    }
+
+    struct Ciphertext {
+        BLS.PointG2 u;
+        bytes v;
+        bytes w;
+    }
+
+    // Decryption request stores details for each decryption request
+    struct DecryptionRequest {
+        string schemeID; // signature scheme id, e.g., "BN254", "BLS12-381", "TESS"
         bytes ciphertext;
+        bytes condition;
+        bytes decryptionKey;
         bytes signature;
         address callback;
     }
